accd525f85cffdec23bf2d0920b198d81160ed84
Alan Bai <59358983+bjm212501@users.noreply.github.com>
2020-09-11 20:39:30 +0800

    Add files via upload

---
+++ google-searchbox/index.js
@@ -0,0 +1,15 @@
+function search()
+ {
+
+    if (document.getElementById("search_input").value != "")
+ {
+
+        window.location.href = "https://www.google.com/search?q=" + document.getElementById("search_input").value;
+
+        document.getElementById("search_input").value = "";
+
+    }
+
+    return false;
+
+}
