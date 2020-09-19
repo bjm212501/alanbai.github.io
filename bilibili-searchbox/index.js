d0f38511e4e2dae360a29f871784f299ffeffc4c
Alan Bai <59358983+bjm212501@users.noreply.github.com>
2020-09-19 16:29:17 +0800

    Update index.js

--- bilibili-searchbox/index.js
+++ bilibili-searchbox/index.js
@@ -4,7 +4,7 @@ function search()
     if (document.getElementById("search_input").value != "")
  {
 
-        window.location.href = "https://www.google.com/search?q=" + document.getElementById("search_input").value;
+        window.location.href = "https://search.bilibili.com/all?keyword=" + document.getElementById("search_input").value;
 
         document.getElementById("search_input").value = "";
 
